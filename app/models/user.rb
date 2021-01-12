6170d0ddfd3de861ac7cc8533eb50139c324a705
NUKEDO <nukedo.4444.orz@gmail.com>
2021-01-12 12:24:50 +0900

    バリデーション設定、ビュー作成(途中)
    PC再起動のため、いったん保存
    トップページ
    新規登録ページ
    ログインページ

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :password,    presence: true
+  validates :nickname_a,  presence: true
 end
